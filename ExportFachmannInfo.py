72b506d73759ef658eb5409b8d4a61fab3514a8c
Dirk Maucher <dirk.maucher@novatec-gmbh.de>
2020-05-05 13:44:24 +0200

    Reworked to make it usable within HomeAssistant, updated documentation, ...

--- ExportFachmannInfo.py
+++ ExportFachmannInfo.py
@@ -1,11 +1,9 @@
 import os
 import time
+import json
 from threading import Lock
 from timeit import default_timer as timer
 
-from prometheus_client import start_http_server, PLATFORM_COLLECTOR, PROCESS_COLLECTOR, GC_COLLECTOR
-from prometheus_client.core import GaugeMetricFamily, REGISTRY
-from prometheus_client.metrics_core import InfoMetricFamily, CounterMetricFamily
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException, WebDriverException
 from selenium.webdriver.chrome.options import Options
@@ -13,54 +11,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
+debug = False
+
 MAP_METRICS = {
-    'Zeitstempel': {'name': 'timestamp', 'type': 'info'},
-    'Außentemperatur': {'name': 'current_outside_temperature_celsius', 'type': 'gauge', 'strip': len(' °C')},
-    'AT Mittelwert': {'name': 'average_outside_temperature_celsius', 'type': 'gauge', 'strip': len(' °C')},
-    'AT Langzeitwert': {'name': 'longtime_outside_temperature_celsius', 'type': 'gauge', 'strip': len(' °C')},
-    'Raumsolltemperatur': {'name': 'room_set_temperature_celsius', 'type': 'gauge', 'strip': len(' °C')},
-    'Vorlaufsolltemperatur': {'name': 'water_inlet_set_temperature_celsius', 'type': 'gauge', 'strip': len(' °C')},
-    'Vorlauftemperatur': {'name': 'water_inlet_temperature_celsius', 'type': 'gauge', 'strip': len(' °C')},
-    'Warmwassertemperatur': {'name': 'hot_water_temperature_celsius', 'type': 'gauge', 'strip': len(' °C')},
-    'Leistungsanforderung': {'name': 'performance_request_ratio', 'type': 'gauge', 'strip': len(' %')},
-    'Schaltdifferenz dynamisch': {'name': 'dynamic_switch_temperature_difference_kelvin', 'type': 'gauge',
-                                  'strip': len(' K')},
-    'LWT': {'name': 'lwt_temperature_celsius', 'type': 'gauge', 'strip': len(' °C')},
-    'Rücklauftemperatur': {'name': 'water_outlet_temperature_celsius', 'type': 'gauge', 'strip': len(' °C')},
-    'Drehzahl Pumpe': {'name': 'pump_rotation_ratio', 'type': 'gauge', 'strip': len(' %')},
-    'Volumenstrom': {'name': 'volume_flow_cubicmeter_per_hour', 'type': 'gauge', 'strip': len('m3/h')},
-    'Stellung Umschaltventil': {'name': 'crossover_valve_setting', 'type': 'info'},
-    'Soll Frequenz Verdichter': {'name': 'set_frequency_compressor_hertz', 'type': 'gauge', 'strip': len(' Hz')},
-    'Ist Frequenz Verdichter': {'name': 'frequency_compressor_hertz', 'type': 'gauge', 'strip': len(' Hz')},
-    'Luftansaugtemperatur': {'name': 'air_inlet_temperature_celsius', 'type': 'gauge', 'strip': len(' °C')},
-    'Wärmetauscher AG Eintritt': {'name': 'outside_heat_exchanger_inlet_temperature_celsius', 'type': 'gauge',
-                                  'strip': len(' °C')},
-    'Wärmetauscher AG Mitte': {'name': 'outside_heat_exchanger_middle_temperature_celsius', 'type': 'gauge',
-                               'strip': len(' °C')},
-    'Druckgas': {'name': 'pressure_gas_temperature_celsius', 'type': 'gauge', 'strip': len(' °C')},
-    'Wärmetauscher Innen': {'name': 'inside_heat_exchanger_temperature_celsius', 'type': 'gauge', 'strip': len(' °C')},
-    'Kältemittel Innen': {'name': 'refrigerant_inside_temperature_celsius', 'type': 'gauge', 'strip': len(' °C')},
-    'Betriebsstd. Verdichter': {'name': 'compressor_operating_hours', 'type': 'gauge', 'strip': len(' h')},
-    'Schaltspiele Verdichter': {'name': 'compressor_cycles', 'type': 'counter'},
-    'Schaltspiele Abtauen': {'name': 'defrosting_cycles', 'type': 'counter'},
-    'Status E-Heizung 1': {'name': 'heating1_state', 'type': 'gauge'},
-    'Status E-Heizung 2': {'name': 'heating2_state', 'type': 'gauge'},
-    'Betriebsstunden E1': {'name': 'heating1_operating_hours', 'type': 'gauge', 'strip': len(' h')},
-    'Betriebsstunden E2': {'name': 'heating2_operating_hours', 'type': 'gauge', 'strip': len(' h')},
-    'Schaltspiele E1': {'name': 'heating1_cycles', 'type': 'counter'},
-    'Schaltspiele E2': {'name': 'heating2_cycles', 'type': 'counter'},
-    'Gesamt Energie Tage': {'name': 'total_energy_per_day', 'type': 'gauge', 'strip': len(' KWh')},
-    'Gesamt Energie Monate': {'name': 'total_energy_per_month', 'type': 'gauge', 'strip': len(' KWh')},
-    'Gesamt Energie Jahre': {'name': 'total_energy_per_year', 'type': 'gauge', 'strip': len(' KWh')},
-    'Heizen Energie Tage': {'name': 'heating_energy_per_day', 'type': 'gauge', 'strip': len(' KWh')},
-    'Heizen Energie Monat': {'name': 'heating_energy_per_month', 'type': 'gauge', 'strip': len(' KWh')},
-    'Heizen Energie Jahre': {'name': 'heating_energy_per_year', 'type': 'gauge', 'strip': len(' KWh')},
-    'WW Energie Tag': {'name': 'water_energy_per_day', 'type': 'gauge', 'strip': len(' KWh')},
-    'WW Energie Monat': {'name': 'water_energy_per_month', 'type': 'gauge', 'strip': len(' KWh')},
-    'WW Energie Jahr': {'name': 'water_energy_per_year', 'type': 'gauge', 'strip': len(' KWh')},
-    'Kühlen Energie Tage': {'name': 'cooling_energy_per_day', 'type': 'gauge', 'strip': len(' KWh')},
-    'Kühlen Energie Monate': {'name': 'cooling_energy_per_month', 'type': 'gauge', 'strip': len(' KWh')},
-    'Kühlen Energie Jahre': {'name': 'cooling_energy_per_year', 'type': 'gauge', 'strip': len(' KWh')},
+    "Zeitstempel": {"name": "timestamp", "type": "info"},
+    "Außentemperatur Aktuell": {"name": "aussentemperatur_aktuell", "type": "gauge", "strip": len(" °C")},
+    "Warmwassertemperatur Aktuell": {"name": "warmwassertemperatur_aktuell", "type": "gauge", "strip": len(" °C")},
 }
 
 chrome_options = Options()
@@ -69,7 +25,8 @@ chrome_options.add_argument("--disable-gpu")
 
 
 def refresh_page(driver):
-    print("Refreshing page...")
+    if (debug):
+        print("Refreshing page...")
     WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.ID, "ctl00_DeviceContextControl1_RefreshDeviceDataButton"))
     ).click()
@@ -77,17 +34,19 @@ def refresh_page(driver):
 
 
 def login_and_load_fachmann_page(driver):
-    wemportal_user = os.environ['WEMPORTAL_USER']
-    wemportal_password = os.environ['WEMPORTAL_PASSWORD']
-    fachmann_password = os.environ['FACHMANN_PASSWORD']
+    wemportal_user = "YOUR_USERNAME"
+    wemportal_password = "YOUR_PASSWORD"
+    fachmann_password = "11"
 
     driver.get("https://www.wemportal.com/Web/")
-    print("Logging in...")
+    if (debug):
+        print("Logging in...")
     driver.find_element(By.ID, "ctl00_content_tbxUserName").click()
     driver.find_element(By.ID, "ctl00_content_tbxUserName").send_keys(wemportal_user)
     driver.find_element(By.ID, "ctl00_content_tbxPassword").send_keys(wemportal_password)
     driver.find_element(By.ID, "ctl00_content_btnLogin").click()
-    print("Go to Fachmann info page...")
+    if (debug):
+        print("Go to Fachmann info page...")
     driver.find_element(By.CSS_SELECTOR, "#ctl00_RMTopMenu > ul > li.rmItem.rmFirst > a > span").click()
     driver.find_element(By.CSS_SELECTOR, "#ctl00_SubMenuControl1_subMenu > ul > li:nth-child(4) > a > span").click()
     driver.switch_to.frame(0)
@@ -100,67 +59,81 @@ def login_and_load_fachmann_page(driver):
 
 
 def wait_until_page_loaded(driver):
+    timeout = time.time() + 60*2   # 2 minutes from now
     while True:
         refresh_button_span = driver.find_element(By.ID, "ctl00_DeviceContextControl1_RefreshDeviceDataButton")
-        print("Waiting for page loaded...".format(refresh_button_span.id), end="")
+        if (debug):
+            print("Waiting for page loaded...".format(refresh_button_span.id), end="")
         start = timer()
         try:
             WebDriverWait(driver, 8, poll_frequency=0.2).until(
                 EC.staleness_of(refresh_button_span)
             )
-            print("took {}".format(timer() - start))
+            if (debug):
+                print("took {}".format(timer() - start))
         except TimeoutException:
-            print("timed out")
+            if (debug):
+                print("timed out")
             break
-    print("Page loaded")
+        if (time.time() > timeout):
+            if (debug):
+                print("Data got not loaded within 2 minutes, stopping it!")
+            break
+    if (debug):
+        print("Page loaded")
 
 
 def parse_page(driver):
     timestamp = driver.find_element(By.ID, "ctl00_DeviceContextControl1_lblDeviceLastDataUpdateInfo").text
     result = {"Zeitstempel": timestamp}
-    print("Parsing page with timestamp {}".format(timestamp))
+    if (debug):
+        print("Parsing page with timestamp {}".format(timestamp))
 
     map_id_to_name = {}
 
     for element in driver.find_elements(By.CLASS_NAME, "simpleDataName"):
-        stripped_id = element.get_attribute('id')[:-8]
+        stripped_id = element.get_attribute("id")[:-8]
         value = element.text
         map_id_to_name[stripped_id] = value
 
     for element in driver.find_elements(By.CLASS_NAME, "simpleDataValue"):
-        stripped_id = element.get_attribute('id')[:-9]
-        value = element.text
+        stripped_id = element.get_attribute("id")[:-9]
+        value = element.text.replace(",", ".")
         result[map_id_to_name[stripped_id]] = value
-    print("Found {} data points".format(len(result)))
+    if (debug):
+        print("Found {} data points".format(len(result)))
+
     return result
 
 
 def parse_value(value, strip=None):
-    if value == 'Aus' or value == '--':
+    if value == "Aus" or value == "--":
         return 0
     elif strip is not None:
         return float(value[:-int(strip)])
     else:
         return float(value)
 
-
 def collect_metrics_from_page(driver):
     result = parse_page(driver)
+
+    data = {}
 
     for key, value in result.items():
         metric = MAP_METRICS.get(key)
         if metric is not None:
-            name = 'wemportal_' + metric['name']
-            t = metric.get('type', 'gauge')
-            if t is 'gauge':
-                value = parse_value(value, metric.get('strip'))
-                yield GaugeMetricFamily(name, key, value=value)
-            if t is 'counter':
-                value = parse_value(value, metric.get('strip'))
-                yield CounterMetricFamily(name, key, value=value)
-            if t is 'info':
-                yield InfoMetricFamily(name, key, value={'value': value})
+            name = metric["name"]
+            t = metric.get("type", "gauge")
+            if t is "gauge":
+                value = parse_value(value, metric.get("strip"))
+                data[name] = value
+            if t is "counter":
+                value = parse_value(value, metric.get("strip"))
+                data[name] = value
+            if t is "info":
+                data[name] = value
 
+    return json.dumps(data)
 
 class CustomCollector(object):
     def __init__(self):
@@ -181,17 +154,21 @@ class CustomCollector(object):
         try:
             self.ensure_driver_restarted()
             self.ensure_refreshed()
-            metrics = list(collect_metrics_from_page(self.driver))
-            print("Exporting {} metrics".format(len(metrics)))
+            metrics = collect_metrics_from_page(self.driver)
+            if (debug):
+                print("Exporting {} metrics".format(len(metrics)))
             self.collections_done = self.collections_done + 1
             return metrics
         except WebDriverException as e:
-            print("Encountered web driver exception:")
-            print(e)
+            if (debug):
+                print("Encountered web driver exception:")
+                print(e)
             if retries_left == 0:
-                print("No retries left, bailing out")
+                if (debug):
+                    print("No retries left, bailing out")
                 raise e
-            print("Restarting driver... (retries_left={})".format(retries_left))
+            if (debug):
+                print("Restarting driver... (retries_left={})".format(retries_left))
             self.restart_driver()
             return self.collect_metrics(retries_left - 1)
         finally:
@@ -201,7 +178,8 @@ class CustomCollector(object):
         if self.collections_done <= 200:
             return
         try:
-            print("Restarting driver as {} collections done".format(self.collections_done))
+            if (debug):
+                print("Restarting driver as {} collections done".format(self.collections_done))
             self.restart_driver()
         finally:
             self.collections_done = 0
@@ -222,15 +200,17 @@ class CustomCollector(object):
         self.refreshed = True
 
     def __del__(self):
-        print("Shutting down...")
-        self.driver.quit()
+        if (debug):
+            print("Shutting down...")
+        try:
+            self.driver.quit()
+        except Exception:
+            pass
+        finally:
+            os.system('pkill chromedriver')
 
 
 if __name__ == "__main__":
-    for c in [PROCESS_COLLECTOR, PLATFORM_COLLECTOR, GC_COLLECTOR]:
-        REGISTRY.unregister(c)
-    REGISTRY.register(CustomCollector())
-    start_http_server(8000)
-    print("Running...")
-    while True:
-        time.sleep(100)
+    customcollector = CustomCollector()
+    metrics = customcollector.collect_metrics()
+    print(metrics)
--- README.md
+++ README.md
@@ -1,13 +1,53 @@
 # WEM Portal Scripts
 
+Fork of neiser and modified to be used within HomeAssistant.
+
 Scripts for Weishaupt WEM Portal using Python and Selenium (and Chromium Headless).
 
-## Install 
+## 3-step Installation
+
+### On your HomeAssistant instance
+
+You need to install the chromium-driver and also the Python selenium package. Commands may be different on your system, just use them as an idea.
 
 ```bash
-apt-get install chromium-driver python3-venv virtualenv
+apt-get install chromium-driver
 
-virtualenv -p python3 venv
-source venv/bin/activate
-pip install -r requirements.txt
+sudo -u homeassistant -H -s
+
+pip3 install selenium
+```
+
+### Script
+
+Within the script **ExportFachmannInfo.py** search for **YOUR\_USERNAME** and **YOUR\_PASSWORD** and replace it with your data.
+
+Put the changed file **ExportFachmannInfo.py** into your HomeAssistant home directory (.homeassistant) within the folder **scripts**.
+
+### Sensor definition
+
+```sensor:
+  - platform: command_line
+    name: heating
+    json_attributes:
+      - "aussentemperatur_aktuell"
+      - "warmwassertemperatur_aktuell"
+    value_template: '{{ value_json.timestamp }}'
+    command: 'python3 /home/homeassistant/.homeassistant/scripts/ExportFachmannInfos.py'
+    scan_interval: 600
+    command_timeout: 180
+  - platform: template
+    sensors:
+      heating_aussentemperatur_aktuell:
+        friendly_name: "Heizung - Außentemperatur Aktuell"
+        value_template: "{{ states.sensor.heating.attributes.aussentemperatur_aktuell }}"
+        unit_of_measurement: '°C'
+        device_class: 'temperature'
+  - platform: template
+    sensors:
+      heating_warmwassertemperatur_aktuell:
+        friendly_name: "Heizung Warmwassertemperatur Aktuell"
+        value_template: "{{ states.sensor.heating.attributes.warmwassertemperatur_aktuell }}"
+        unit_of_measurement: '°C'
+        device_class: 'temperature'
 ```
